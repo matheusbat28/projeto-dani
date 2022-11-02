e7b7fb3e870351f0104f0f6629ca5cea4e04d392
matheusbat28 <matheusarrow28@gmail.com>
2022-11-02 19:57:30 -0300

    mascara cadastro

--- vendor/js/cadastro/scripts.js
+++ vendor/js/cadastro/scripts.js
@@ -1,3 +1,12 @@
-function entrarLogin() {
-    window.location = '../login/index.html'
-}
+const handlePhone = (event) => {
+    let input = event.target
+    input.value = phoneMask(input.value)
+  }
+  
+  const phoneMask = (value) => {
+    if (!value) return ""
+    value = value.replace(/\D/g,'')
+    value = value.replace(/(\d{2})(\d)/,"($1) $2")
+    value = value.replace(/(\d)(\d{4})$/,"$1-$2")
+    return value
+  }
