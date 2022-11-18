8cf122414790ad25fbc695ee37433535131e4731
matheusbat28 <matheusarrow28@gmail.com>
2022-11-18 18:04:58 -0300

    asdasd

--- config/settings.py
+++ config/settings.py
@@ -78,17 +78,17 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'd46578f000m6b0',
-        'HOST': 'ec2-3-216-167-65.compute-1.amazonaws.com',
-        'PORT': '5432',
-        'USER': 'ziyskadaptxzhm',
-        'PASSWORD': '55be270ea7612d16f46f1684428a173646416337499b6555dd355a06bc2ddc61',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'loja',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'USER': 'root',
+        'PASSWORD': '0506',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
