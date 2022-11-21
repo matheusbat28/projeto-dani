d015c6f98f66c91fa73f2a6026ec21578a36db84
matheusbat28 <matheusarrow28@gmail.com>
2022-11-20 21:42:55 -0300

    add tela perfil, campo de pesquisa de produto, add home

--- inicio/urls.py
+++ inicio/urls.py
@@ -5,6 +5,9 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('logout', views.logout, name="logout"),
     path('categoria', views.categoria, name="categoria"),
+    path('produtos', views.produtos, name="produtos"),
     path('carrinho', views.carrinho, name="carrinho"),
     path('deletarCart/<int:id>/', views.deletarCart, name='deletarCart'),
+    path('perfil/', views.perfil, name='perfil'),
+    path('buscar/', views.buscar, name='buscar'),
 ]
--- inicio/views.py
+++ inicio/views.py
@@ -2,13 +2,22 @@ from django.shortcuts import render, redirect
 from django.contrib import auth
 from django.contrib.auth.decorators import login_required
 from .models import Produto, Categoria, Carrinho
+from conta.models import Usuario
+from django.contrib.auth.hashers import make_password
+from django.contrib import messages
+from django.db.models import Q
 
 @login_required(login_url= 'login')
 def home(request):
+    context = {
+        'produtos': Produto.objects.all(),
+        'categorias': Categoria.objects.all(),
+    }
     if request.method == "POST":
         pass
     else:
-        return render(request, 'home/index.html')
+
+        return render(request, 'home/index.html', context=context)
 
 @login_required(login_url= 'login')
 def logout(request):
@@ -42,8 +51,12 @@ def carrinho(request):
         
         for carrinho in carrinhos:  
             valores = '{:.2f}'.format(carrinho.produto.preco * carrinho.quantidade_produto)
+            produto = Produto.objects.get(id = carrinho.produto.id)
+            produto.quantidade -= carrinho.quantidade_produto
+
             carrinho.status = True
             carrinho.valorTotal = valores
+            produto.save()
             carrinho.save()
         
         return redirect('carrinho')
@@ -57,3 +70,66 @@ def deletarCart(request, id):
         return redirect('carrinho')
     else:
         return redirect('carrinho')
+    
+@login_required(login_url= 'login')
+def produtos(request):
+    context = {
+        'produtos': Produto.objects.all(),
+    }
+    if request.method == "POST":
+        produt = Produto.objects.get(id = request.POST.get('btCart'))
+        qtdProduto = request.POST.get('qtdProduto')
+        Carrinho.objects.create(produto_id = produt.id, usuario_id=request.user.id, quantidade_produto=qtdProduto)
+        return redirect('produtos')
+    else:
+        return render(request, 'produtos/index.html', context=context)
+    
+@login_required(login_url= 'login')
+def perfil(request):
+    usuario = Usuario.objects.get(id=request.user.id)
+    if request.method == "POST":
+        msg = ''
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        telefone = request.POST.get('telefone')
+        email = request.POST.get('email')
+        senha1 = request.POST.get('senha1')
+        senha2 = request.POST.get('senha2')
+        img = request.FILES.get('image')
+        if first_name != usuario.first_name:
+            usuario.first_name = first_name
+            msg +="Usuario,"
+        if last_name != usuario.last_name:
+            usuario.last_name = last_name  
+            msg +="Ultimo Nome, " 
+        if telefone != usuario.telefone:
+            usuario.telefone = telefone 
+            msg +="Telefone, "    
+        if email != usuario.email:
+            usuario.email = email  
+            msg +="Email,"   
+        if img != '':
+            usuario.perfil_img = img    
+            msg +="Foto,"   
+        if senha1 == senha2 and senha1 != '' and senha2 != '':
+            if len(senha1) <= 8 and len(senha1) >=4:
+                usuario.password = make_password(senha1)
+                usuario.save()
+                msg +="Senha," 
+                
+            else:    
+                usuario.save()
+        usuario.save()  
+        messages.success(request , msg + ' - Foram Salvos')              
+        return redirect('perfil')
+    else:
+
+        return render(request, 'perfil/index.html', {'usuario':usuario})        
+
+@login_required(login_url= 'login')
+def buscar(request):
+    busca = request.GET.get('buscar')
+    context = {
+        'produtos': Produto.objects.filter(Q(nome__icontains=busca) | Q(descricao__icontains=busca)),
+    }
+    return render(request, 'produtos/index.html', context=context)
